bd0d5a4363649afbbf28aabadb367a3ef97ee475
JP Richardson <jprichardson@gmail.com>
2012-11-13 22:24:42 -0600

    Initial release.

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 Node.js - qflow
 ================
 
-Type description here...
+A very simple data queue processing library.
+
+ Not to be confused with [queue-flow]() ([David Elli]
 
 
 Why?
 ----
+
+[BatchFlow](https://github.com/jprichardson/node-batchflow) didn't cut it because I needed to keep batch processing a data set.
+
+
+
+What About queue-flow?
+----------------------
+
+Do not confuse this (`qflow`) with the great library [queue-flow](http://dfellis.github.com/queue-flow/2012/09/21/tutorial/) by [David Ellis](http://dfellis.posterous.com/). `queue-flow` does much more and very powerful, you should check it out. I wanted something simpler for my needs. I unfortunatley chose a the name `qflow` when `queue-flow` was already taken. I did this because I wanted to keep a cohesive naming scheme with my other `flow` libraries: [NextFlow](https://github.com/jprichardson/node-nextflow), [BatchFlow](https://github.com/jprichardson/node-batchflow), and [TriggerFlow](https://github.com/jprichardson/node-triggerflow).
 
 
 
@@ -19,9 +30,35 @@ Installation
 Example
 ------
 
+Super simple example:
+
 
 ```javascript
+var results = []
+qflow([1,2,3,4])
+.deq(function(val, next) {
+  results.push(val * 2)
+  next()
+})
+.on('error', function(err) {
+  console.error(err)
+})
+.on('empty', function() {
+  T (results[0] === 2)
+  T (results[1] === 4)
+  T (results[2] === 6)
+  T (results[3] === 8)
+  T (results.length === 4)
+  done()
+})
+.start(1) //essentially process sequentially.
 ```
+
+Methods
+-------
+
+Todo. In the meantime, use the source Luke.
+
 
 License
 -------
--- test/qflow.test.js
+++ test/qflow.test.js
@@ -1,11 +1,11 @@
-var q = require('../lib/qflow')
+var qflow = require('../lib/qflow')
   , testutil = require('testutil')
 
 describe('qflow', function () {
   describe('> when collection is passed in and no more are added and a limit is set to 1', function() {
     it('should process the items', function(done) {
       var results = []
-      q([1,2,3,4])
+      qflow([1,2,3,4])
       .deq(function(val, next) {
         results.push(val * 2)
         next()
@@ -36,7 +36,7 @@ describe('qflow', function () {
         function(done) { setTimeout(function() { res.push(4); done() }, 12) }
       ]
 
-      q(data)
+      qflow(data)
       .deq(function(val, next) {
         val(next)
       })
@@ -56,5 +56,163 @@ describe('qflow', function () {
     })
   })
 
+  describe('> when more items are queued up and limit is 2', function(done) {
+    it ('should process the items', function(done) {
+      var res = []
+      var data = [
+        function(done) { setTimeout(function() { res.push(1); done() }, 15) }, 
+        function(done) { setTimeout(function() { res.push(2); done() }, 30) }, 
+        function(done) { setTimeout(function() { res.push(3); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(4); done() }, 60) }
+      ]
+
+      var data2 = [
+        function(done) { setTimeout(function() { res.push(5); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(6); done() }, 20) }, 
+        function(done) { setTimeout(function() { res.push(7); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(8); done() }, 2) }
+      ]
+
+      var q = qflow(data)
+      .deq(function(val, next) {
+        val(next)
+      })
+      .on('error', function() {
+        F (err)
+        T (false)
+      })
+      .on('empty', function() {
+        T (res[0] === 1)
+        T (res[1] === 3)
+        T (res[2] === 2)
+        T (res[3] === 5)
+        T (res[4] === 6)
+        T (res[5] === 7)
+        T (res[6] === 8)
+        T (res[7] === 4)
+        T (res.length === 8)
+        done()
+      })
+      .start(2)
+
+      data2.forEach(function(fun) {
+        q.enq(fun)
+      })
+
+    })
+  })
+
+  describe('> when queue goes empty and more items are added', function() {
+    it ('should start again and process the new items', function(done) {
+      var res = []
+      var data = [
+        function(done) { setTimeout(function() { res.push(1); done() }, 15) }, 
+        function(done) { setTimeout(function() { res.push(2); done() }, 30) }, 
+        function(done) { setTimeout(function() { res.push(3); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(4); done() }, 60) }
+      ]
+
+      var data2 = [
+        function(done) { setTimeout(function() { res.push(5); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(6); done() }, 20) }, 
+        function(done) { setTimeout(function() { res.push(7); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(8); done() }, 2) }
+      ]
+
+      var emptyCount = 0;
+
+      var q = qflow(data)
+      .deq(function(val, next) {
+        val(next)
+      })
+      .on('error', function() {
+        F (err)
+        T (false)
+      })
+      .on('empty', function() {
+        if (emptyCount === 0) {
+          data2.forEach(function(fun) {
+            q.enq(fun)
+          })
+
+          emptyCount += 1
+        } else {
+          T (res[0] === 3)
+          T (res[1] === 1)
+          T (res[2] === 2)
+          T (res[3] === 4)
+          T (res[4] === 5)
+          T (res[5] === 7)
+          T (res[6] === 8)
+          T (res[7] === 6)
+          T (res.length === 8)
+          done()
+        }
+       
+      })
+      .start(3)
+    })
+  })
+
+  describe('> when an error is thrown or an error is passed as first param', function() {
+    it ('should emit the error event', function(done) {
+      var res = []
+      var data = [
+        function(done) { throw new Error('1')  },
+        function(done) { setTimeout(function() { done(new Error('2')) }, 2) } 
+      ]
+
+      var errorCount = 0
+
+      var q = qflow(data)
+      .deq(function(val, next) {
+        //console.log('VAL: ' + val)
+        val(next)
+      })
+      .on('error', function(err) {
+        T (err)
+        errorCount += 1
+      })
+      .on('empty', function() {
+        T (errorCount === 2)
+        done()
+      })
+      .start(2)
+    })
+  })
+
+  describe('> when paused is called', function() {
+    it ('should finish the current running operations and emit the pause event', function(done) {
+      var res = []
+      var data = [
+        function(done) { setTimeout(function() { res.push(1); done() }, 15) }, 
+        function(done) { setTimeout(function() { res.push(2); done() }, 30) }, 
+        function(done) { setTimeout(function() { res.push(3); done() }, 5) }, 
+        function(done) { setTimeout(function() { res.push(4); done() }, 12) }
+      ]
+
+      var q = qflow(data)
+      q.deq(function(val, doneFun) {
+        q.pause()
+        val(doneFun)
+      })
+      .on('error', function(err) {
+        console.error(err)
+        F (err)
+        T (false)
+      })
+      .on('empty', function() {
+        console.dir(res)
+        T (false) //should never get here
+      })
+      .on('paused', function() {
+        T (res[0] === 1)
+        T (res[1] === 2)
+        T (res.length === 2)
+        done()
+      })
+      .start(2)
+    })
+  })
 
 })
